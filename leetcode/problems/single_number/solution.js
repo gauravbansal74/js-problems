6dfccef7fc0a96816b3987354ca0552fc868ef86
Gaurav Kumar <gauravbansal74@gmail.com>
2020-12-05 20:39:37 +0000

    Sync LeetCode submission - Single Number (javascript)

---
+++ leetcode/problems/single_number/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let set = new Set();
+
+  for (num of nums) {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+
+  return Array.from(set)[0];
+};
